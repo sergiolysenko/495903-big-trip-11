d67b8796689322800c747f0a4de4fef4c4acdb56
sergiolysenko <54682953+sergiolysenko@users.noreply.github.com>
2020-05-06 17:26:44 +0300

    при различной сортировке перерисовывается разметка. Показ и удаления элемента day

--- src/components/main-trip-sort.js
+++ src/components/main-trip-sort.js
@@ -1,4 +1,4 @@
-import {AbstractComponent} from "./abstractComponent.js";
+import {AbstractSmartComponent} from "./abstractSmartComponent.js";
 
 const SortType = {
   EVENT: `event`,
@@ -7,18 +7,21 @@ const SortType = {
 };
 
 const createMainTripSortTemplate = (currentSortType) => {
-  const isCurrentSortEvent = currentSortType === SortType.EVENT ? `Day` : ``;
+  const isCurrentSortEvent = currentSortType === SortType.EVENT;
+  const isCurrentTypeTime = currentSortType === SortType.TIME;
+  const isCurrentTypePrice = currentSortType === SortType.PRICE;
   return (`<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       
-      <span class="trip-sort__item  trip-sort__item--day">${isCurrentSortEvent}</span>
+      <span class="trip-sort__item  trip-sort__item--day">${isCurrentSortEvent ? `Day` : ``}</span>
       
       <div class="trip-sort__item  trip-sort__item--event">
-        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" checked>
+        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio"
+        name="trip-sort" value="sort-event" ${isCurrentSortEvent ? `checked` : ``}>
         <label data-sort-type="${SortType.EVENT}" class="trip-sort__btn" for="sort-event">Event</label>
       </div>
 
       <div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time" ${isCurrentTypeTime ? `checked` : ``}>
         <label data-sort-type="${SortType.TIME}" class="trip-sort__btn" for="sort-time">
           Time
           <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
@@ -28,7 +31,7 @@ const createMainTripSortTemplate = (currentSortType) => {
       </div>
 
       <div class="trip-sort__item  trip-sort__item--price">
-        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" ${isCurrentTypePrice ? `checked` : ``}>
         <label data-sort-type="${SortType.PRICE}" class="trip-sort__btn" for="sort-price">
           Price
           <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
@@ -41,14 +44,22 @@ const createMainTripSortTemplate = (currentSortType) => {
     </form>`);
 };
 
-class MainTripSortComponent extends AbstractComponent {
+class MainTripSortComponent extends AbstractSmartComponent {
   constructor() {
     super();
     this._currentTypeSort = SortType.EVENT;
+    this.sortTypeChangeHandler = null;
   }
 
   getTemplate() {
-    return createMainTripSortTemplate();
+    return createMainTripSortTemplate(this._currentTypeSort);
+  }
+
+  rerender() {
+    super.rerender();
+  }
+  recoveryListeners() {
+    this.setSortTypeChangeHandler(this.sortTypeChangeHandler);
   }
 
   getSortType() {
@@ -66,8 +77,10 @@ class MainTripSortComponent extends AbstractComponent {
       if (this._currentTypeSort === sortType) {
         return;
       }
+      this.sortTypeChangeHandler = handler;
       this._currentTypeSort = sortType;
       handler(this._currentTypeSort);
+      this.rerender();
     });
   }
 }
